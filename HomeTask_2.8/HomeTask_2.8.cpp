4d4094487ce58c7c51948f577d61fd5a6f9448d2
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-06-15 10:51:29 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_2.8/HomeTask_2.8.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int a, sum = 0;
+	cin >> a;
+	for (int i = a; i > 0; i /= 10)
+	{
+		sum += i % 10;
+	}
+
+	if (sum * sum * sum == a * a) cout << "Равно А * А " << endl;
+	else cout << "Не равно А * А" << endl;
+}
+
